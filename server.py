35610c2a69a47c988f1ca350128dfe4f0723d1b4
jessicamow <jessicammow@gmail.com>
2022-04-14 16:21:29 -0700

    Finished with lab, moving on to further study

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
-from pprint import pformat
+import json
+from pprint import pformat, pprint
 import os
 import requests
 
@@ -41,8 +42,17 @@ def find_afterparties():
     sort = request.args.get('sort', '')
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
-    payload = {'apikey': API_KEY}
+    payload = {
+        'apikey': API_KEY, 
+        'keyword': keyword, 
+        'postalCode': postalcode,
+        'radius': radius,
+        'unit': unit,
+        'sort': sort
+    } 
+    
 
+    res = requests.get(url, params=payload)
     # TODO: Make a request to the Event Search endpoint to search for events
     #
     # - Use form data from the user to populate any search parameters
@@ -54,9 +64,8 @@ def find_afterparties():
     # - Replace the empty list in `events` with the list of events from your
     #   search results
 
-    data = {'Test': ['This is just some test data'],
-            'page': {'totalElements': 1}}
-    events = []
+    data = res.json()
+    events = data['_embedded']['events']
 
     return render_template('search-results.html',
                            pformat=pformat,
@@ -74,6 +83,21 @@ def get_event_details(id):
     """View the details of an event."""
 
     # TODO: Finish implementing this view function
+
+    url = ('https://app.ticketmaster.com/discovery/v2/events/' + str(id))
+    payload = {'apikey': API_KEY}
+
+    res = requests.get(url, params=payload)
+    data = res.json()
+    pprint('python data: ', data)
+
+    print('data: ', data)
+    name = data['name']
+    print('name: ', name)
+    image = data['images'][0]['url']
+    print('image: ', image)
+    url = data['url']
+    print('url: ', url)
 
     return render_template('event-details.html')
 
